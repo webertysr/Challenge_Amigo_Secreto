02a4e9d9fb97fe27078b06599b78cebe33437fe3
weberty.souza <weberty.cultura@gmail.com>
2025-03-17 18:27:08 -0300

    Implementação da Função para Atualizar a Lista de Amigos

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ function atualizarLista() {
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = ""; // Limpa a lista antes de atualizar
 
-    amigos.forEach(amigo => {
+    for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement("li");
-        li.textContent = amigo;
+        li.textContent = amigos[i];
         lista.appendChild(li);
-    });
+    }
 }
